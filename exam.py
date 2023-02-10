75a4e0f437584329008839f0945d6af00fee983d
Mattia Monga <mattia.monga@unimi.it>
2023-02-10 15:38:54 +0100

    solution

--- exam.py
+++ exam.py
@@ -51,7 +51,12 @@ import arviz as az   # type: ignore
 #
 # Load the data in a pandas dataframe and make a `bool` column `wooden` which is `True` iff the plant part is "wood" or "bark".
 
-pass
+plants = pd.read_csv('Plant_data.csv', sep=';')
+
+plants['wooden'] = (plants['Plant_part'] == "wood") | (plants['Plant_part'] == "bark")
+
+plants.head()
+
 
 # ### Exercise 2 (max 6 points)
 #
@@ -61,15 +66,62 @@ pass
 # $g(x) = \frac{1}{\sigma\sqrt{2\pi}} \exp\left( -\frac{1}{2} \frac{(x - \mu)^2}{\sigma^2} \right)$
 #
 
-pass
+def gaussian(x: np.ndarray, mu: float, sigma: float) -> np.ndarray:
+    exp = (-1/2) * (x - mu)**2 / sigma**2
+    return (1/sigma*np.sqrt(2*np.pi))*np.exp(exp)
+
+
+
+# +
+sd_max = plants['d15N_sd'].max()
+m_min = plants['d15N'].min()
+m_max = plants['d15N'].max()
+x = np.linspace(m_min-3*sd_max, m_max+3*sd_max, 1000)
+
+fig, ax = plt.subplots(figsize=(15, 10))
+
+for i in plants.index:
+    if not np.isnan(plants.loc[i]['d15N_sd']):
+        if plants.loc[i]['d15N_sd'] > 0:
+            ax.plot(x, gaussian(x,
+                                mu=plants.loc[i]['d15N'],
+                                sigma=plants.loc[i]['d15N_sd']),
+                    label=plants.loc[i]['Species'] + " " + plants.loc[i]['Plant_part'])
+        else:
+            ax.vlines(plants.loc[i]['d15N_sd'], 0, 100)
+_ = fig.legend()
+
+
+# -
 
 # ### Exercise 3 (max 6 points)
 #
-# Define a function `secret_sauce` that takes a plant type (a string), a d13C (a float) and percentage (a float between 0 and 1): the result should be the percentage applied to d13C if the plant type is "C3" and (1 - percentage) of d13C if the plant type is "C4". For example, secret_sauce("C3", 30, .1) should be 3.0, and secret_sauce("C4", 30, .1) should be 27.0. 
+# Define a function `secret_sauce` that takes a plant type (a string), a d13C (a float) and percentage (a float between 0 and 1): the result should be the percentage applied to d13C if the plant type is "C3" and (1 - percentage) of d13C if the plant type is "C4". For example, secret_sauce("C3", 30, .1) should be 3.0, and secret_sauce("C4", 30, .1) should be 27.0.
 #
 # To get the full marks, you should declare correctly the type hints and add a test within a doctest string.
 
-pass
+def secret_sauce(plant_type: str, d13C: float, percentage: float) -> float:
+    """Return the secret_sauce percentage of d13C.
+
+    >>> abs(secret_sauce("C3", 30, .1) - 3.0) < 10e-5
+    True
+
+    >>> abs(secret_sauce("C4", 30, .1) - 27.0) < 10e-5
+    True
+
+    """
+
+    assert 0 <= percentage <= 1
+    if plant_type == "C3":
+        return percentage * d13C
+    if plant_type == "C4":
+        return (1 - percentage) * d13C
+    return float('nan')
+
+
+
+import doctest
+doctest.testmod()
 
 # ### Exercise 4 (max 4 points)
 #
@@ -77,25 +129,43 @@ pass
 #
 # To get the full marks avoid the use of explicit loops.
 
-pass
+plants['secret_sauce'] = plants.apply(lambda row: secret_sauce(row['Plant_type'], row['d13C'], row['C_cont']/100),
+                                      axis=1)
+
+plants.head()
 
 # ### Exercise 5 (max 2 points)
 #
-# Print the mean d15N for each type of plant part. 
+# Print the mean d15N for each type of plant part.
 
-pass
+plants.groupby('Plant_part')['d15N'].mean()
 
 # ### Exercise 6 (max 3 points)
 #
 # Make a scatter plot of `d13C` vs. `d15N`, using different colors for each plant part.
 
-pass
+# +
+fig, ax = plt.subplots()
+
+
+for p in plants['Plant_part'].unique():
+    pp = plants[plants['Plant_part'] == p]
+    ax.scatter(pp['d13C'], pp['d15N'], label=p)
+ax.set_xlabel('d13C')
+ax.set_ylabel('d15N')
+_ = fig.legend()
+
+# -
 
 # ### Exercise 7 (max 3 points)
 #
 # Compute the mean and the standard deviation of the sum of `d13C` and `d15N` when taking into account only the acaciae (all the types included in the data).
 
-pass
+# +
+pp = plants[plants['Species'].str.contains('Acacia')]
+
+(pp['d13C'] + pp['d15N']).describe()[['mean', 'std']]
+# -
 
 # ### Exercise 8 (max 6 points)
 #
@@ -104,7 +174,7 @@ pass
 # - a parameter $\alpha$ is normally distributed with mean 0 and standard deviation 5
 # - a parameter $\beta$ is normally distributed with mean 0 and standard deviation 5
 # - $\sigma$ is exponentially distributed with $\lambda = 1$
-# - the observed `N_cont` is normally distributed with a mean given by $\alpha + \beta\cdot C$, where $C$ is the corresponding `C_cont`
+# - the observed `N_cont` is normally distributed with a mean given by $\alpha + \beta\cdot C$, where $C$ is the corresponding `C_cont`, and a standard deviation of $\sigma$
 #
 # Code this model with pymc, sample the model, and plot the summary of the resulting estimation by using `az.plot_posterior`.
 #
@@ -112,5 +182,14 @@ pass
 #
 #
 
-pass
+with pm.Model():
 
+    a = pm.Normal('alpha', 0, 5)
+    b = pm.Normal('beta', 0, 5)
+    s = pm.Exponential('sigma', 1)
+
+    pm.Normal('N_cont', a + b*plants['C_cont'], s, observed=plants['N_cont'])
+    idata = pm.sample()
+
+
+_ = az.plot_posterior(idata)
